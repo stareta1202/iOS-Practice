3bc107bdd9bb8041f29f0b6699c077425fd68a41
Yongjun Lee <stareta1202@gmail.com>
2021-03-02 15:53:16 +0900

    first Scene setting (without Storyboard)

---
+++ iOS/WithoutStoryboard/WithoutStoryboard/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WithoutStoryboard
+//
+//  Created by Yongjun Lee on 2021/03/02.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ iOS/WithoutStoryboard/WithoutStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WithoutStoryboard
+//
+//  Created by Yongjun Lee on 2021/03/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
