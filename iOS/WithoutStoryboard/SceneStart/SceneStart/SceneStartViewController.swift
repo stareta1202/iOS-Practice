18378224e0b4aa9ef6eae86801a6486fd617c078
yongjun lee <stareta1202@gmail.com>
2021-03-02 17:37:07 +0900

    start scene practice

---
+++ iOS/WithoutStoryboard/SceneStart/SceneStart/SceneStartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SceneStartViewController.swift
+//  SceneStart
+//
+//  Created by Yongjun Lee on 2021/03/02.
+//
+
+import UIKit
+
+class SceneStartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
